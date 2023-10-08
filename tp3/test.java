01e7ebda26f9f1268321405edbfe3e81d77dad93
seif2003 <146482636+seif2003@users.noreply.github.com>
2023-10-08 18:40:04 +0100

    Update test.java

--- tp3/test.java
+++ tp3/test.java
@@ -3,15 +3,32 @@ package tp3;
 public class test {
 
 	public static void main(String[] args) {
+		int n = 3;
+		Compte[] T = new Compte[n];
 		Personne p1 = new Personne("seif ddibe","ben amara","rades");
-		Compte c1 = new Compte(p1,1000,500,500);
-		Compte c2 = new Compte(p1,1000,500,500);
-		c1.Virement(100, c2);
-		System.out.println(c1);
-		System.out.println(c2);
-		Compte.Virement(100, c2,c1);
-		System.out.println(c1);
-		System.out.println(c2);
-	}
+		Personne p2 = new Personne("ramzi","ben amara","rades1");
+		Personne p3 = new Personne("mohammed","ben amara","rades2");
+		T[0] = new Compte(p1,1000,500,500);
+		T[1] = new Compte(p2,1200,600,550);
+		T[2] = new Compte(p3,1500,700,600);
+		for (Compte C : T) {
+			System.out.println(C);
+        }
+		System.out.println("Avant virement");
+        System.out.println(T[0].getSolde());
+        System.out.println(T[1].getSolde());
+        Compte.Virement(200,T[0],T[1]);
+        System.out.println("Apres virement");
+        System.out.println(T[0].getSolde());
+        System.out.println(T[1].getSolde());
 
+
+        Compte MaxCompte = T[0];
+        for (Compte compte : T) {
+        	if (compte.getSolde() > MaxCompte.getSolde()) {
+        		MaxCompte = compte;
+        	}
+        }
+        System.out.println("Compte max :"+MaxCompte);
+        }
 }
